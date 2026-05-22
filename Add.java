4517c5e6428396a3121c2bd7b034459ac2d68f68
Kishanr707 <kishanramesh136@gmail.com>
2026-05-22 09:00:22 +0000

    first commit

---
+++ Add.java
@@ -0,0 +1,8 @@
+public class Add{
+    public static void main(String[] args) {
+        int a = 34;
+        int b = 67;
+        int sum = a + b;
+        System.out.println("SUM: " + sum);
+    }
+}
